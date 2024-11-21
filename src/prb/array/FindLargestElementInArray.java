c3229926459515e2c73d813ae67442e5e3176916
atharva11m <atharvamalode5@gmail.com>
2024-11-21 16:36:57 +0530

    1st problem

--- src/prb/array/FindLargestElementInArray.java
+++ src/prb/array/FindLargestElementInArray.java
@@ -2,6 +2,13 @@ package prb.array;
 
 public class FindLargestElementInArray {
     public static void main(String[] args) {
-
+        int [] array={11,44,55,22,44,88,12,34,56};
+        int num=array[0];
+        for(int i=1;i<array.length;i++){
+            if(num<array[i]){
+                num=array[i];
+            }
+        }
+        System.out.println(num);
     }
 }
